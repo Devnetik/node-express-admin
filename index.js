784ddcf708e927c5cfd1e7e2392ddfcb0fab6ac7
Michael Malura <maluramichael@googlemail.com>
2016-11-01 20:22:18 +0100

    Add views

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var express = require('express');
+var express = require('express'),
+  path = require('path');
 
-module.exports = function(options){
+module.exports = function(app, options) {
+  // TODO: merge options with defaults
 
+  // register views directory
+  let views = app.get('views');
+  if (!Array.isArray(views)) {
+    views = [views];
+  }
+  views.push(path.join(__dirname, 'views'));
+  app.set('views', views);
+
+  // create routes
   const router = new express.Router();
-
-  router.get('/', function(req, res, next){
-    console.log('admin req');
-    res.json({});
+  router.get('/', function(req, res, next) {
+    res.render('admin_index', {options: options});
   });
 
-  return router;
+  // register router
+  app.use(options.path || '/admin', router);
 }
